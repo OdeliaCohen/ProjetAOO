bed13f41f085b50877f44f1aefa6a9f623e0e8a1
OdeliaCohen <odeliacohen1503@gmail.com>
2024-03-26 15:17:59 +0100

    accounService for login and bdd

--- src/main/java/com/example/demo/controller/SignController.java
+++ src/main/java/com/example/demo/controller/SignController.java
@@ -18,10 +18,21 @@ public class SignController {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("account", new Account());
         return "connexion"; // Le nom du template sans l'extension .html
     }
-
+    
+    @PostMapping("/login")
+    public String authenticate(@ModelAttribute Account account, Model model) {
+        if (accountService.authenticate(account.getEmail(), account.getPassword())) {
+            // Connectez l'utilisateur ici (par exemple, en utilisant Spring Security)
+            return "accueil"; // Redirigez vers une page de succès ou une page d'accueil
+        } else {
+            model.addAttribute("error", "Email ou mot de passe incorrect.");
+            return "connexion"; // Retournez à la page de connexion avec un message d'erreur
+        }
+    }
     @GetMapping("/signup")
     public String signup(Model model) {
         model.addAttribute("account", new Account());
@@ -47,6 +58,11 @@ public class SignController {
     @GetMapping("/logout")
     public String logout() {
         return "connexion";
+    }
+
+    @PostMapping("/accueil")
+    public String accueil() {
+        return "accueil";
     }
 
 }
--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -26,6 +27,17 @@ public class AccountService {
 
     public List<Account> findAllAccounts() {
         return signRepository.findAll();
+    }
+
+
+      // Méthode pour vérifier si l'email et le mot de passe correspondent dans la base de données
+      public boolean authenticate(String email, String password) {
+        Optional<Account> optionalAccount = signRepository.findByEmail(email);
+        if (optionalAccount.isPresent()) {
+            Account account = optionalAccount.get();
+            return account.getPassword().equals(password); // Vérifiez si le mot de passe correspond
+        }
+        return false; // Si aucun compte n'est trouvé avec cet email
     }
        
 }
