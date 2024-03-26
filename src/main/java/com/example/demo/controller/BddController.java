aaa0612bf53df8d06f99a26c456e8a436795069d
OdeliaCohen <odeliacohen1503@gmail.com>
2024-03-26 01:31:02 +0100

    link template

--- src/main/java/com/example/demo/controller/BddController.java
+++ src/main/java/com/example/demo/controller/BddController.java
@@ -3,11 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Repository.SignRepository;
 import com.example.demo.model.Account;
